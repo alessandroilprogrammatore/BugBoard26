662c3bd6829c26a4061c30f8c2714e03383adbc5
Alessandro Minopoli <137820554+alessandroilprogrammatore@users.noreply.github.com>
2025-11-05 18:30:00 +0100

    feat: inizializzazione progetto backend Spring Boot

---
+++ backend/src/main/java/com/bugboard26/Bugboard26Application.java
@@ -0,0 +1,12 @@
+package com.bugboard26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bugboard26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bugboard26Application.class, args);
+    }
+}
